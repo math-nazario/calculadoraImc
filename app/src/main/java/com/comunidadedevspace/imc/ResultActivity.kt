899857e925b3f0887ced89002dd53bd2e08ffd29
MATHEUS <math.pnazario@gmail.com>
2024-02-29 11:06:50 -0300

    Inclusão Tabela IMC e melhoria da inface do usuário.

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -1,7 +1,9 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
@@ -16,15 +18,36 @@ class ResultActivity : AppCompatActivity() {
         val tvClassificacao = findViewById<TextView>(R.id.tvClassificacao)
         tvResultado.text = resultado.toString()
 
-        val classificacao: String = if (resultado <= 18.5f) {
-            "MAGREZA"
-        } else if (resultado > 18.5f && resultado <= 24.9f) {
-            "NORMAL"
-        } else if (resultado > 25f && resultado <= 39.9f) {
-            "SOBREPESO"
+        val classificacao: String = if (resultado < 18.5f) {
+            "Abaixo do peso"
+        } else if (resultado >= 18.5f && resultado <= 24.9f) {
+            "Peso ideal"
+        } else if (resultado >= 25f && resultado <= 29.9f) {
+            "Sobrepeso"
+        } else if (resultado >= 30f && resultado <= 34.9f) {
+            "Obesidade grau 1"
+        } else if (resultado >= 35f && resultado <= 39.9f) {
+            "Obesidade grau 2 (Severa)"
         } else {
-            "OBSIDADE GRAVE"
+            "Obesidade grau 3 (Mórbida)"
         }
         tvClassificacao.text = classificacao
+
+        val coresClassificacao: Int = when (classificacao) {
+            "Abaixo do peso" -> android.graphics.Color.rgb(72, 61, 139)
+            "Peso ideal" -> android.graphics.Color.rgb(0, 128, 0)
+            "Sobrepeso" -> android.graphics.Color.rgb(255, 215, 0)
+            "Obesidade grau 1" -> android.graphics.Color.rgb(255, 140, 0)
+            "Obesidade grau 2 (Severa)" -> android.graphics.Color.rgb(255, 0, 0)
+            "Obesidade grau 3 (Mórbida)" -> android.graphics.Color.rgb(128, 0, 128)
+            else -> android.graphics.Color.BLACK
+        }
+        tvClassificacao.setTextColor(coresClassificacao)
+
+        val btnTable = findViewById<Button>(R.id.btnTabela)
+        btnTable.setOnClickListener {
+            val intent = Intent(this, TableActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/comunidadedevspace/imc/TableActivity.kt
@@ -0,0 +1,12 @@
+package com.comunidadedevspace.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
